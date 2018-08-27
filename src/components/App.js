feca13e74d66dfccd6462b212f3b96272351ed85
SytnikArtem <artemasitnik@gmail.com>
2018-08-27 13:53:02 +0300

    1
    1

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React, {PureComponent} from 'react'
+import ArticleList from './ArticleList/index.js'
+import articles from '../fixture'
+import 'bootstrap/dist/css/bootstrap.css'
+
+class App extends PureComponent {
+  state = {
+    reverted: false
+  }
+  render () {
+    console.log(1)
+    return (
+      <div className="container">
+      <div className="jumbotron">
+        <h1 className="display-3">App name</h1>
+        <button className="btn" onClick = {this.revert}>Revert</button>
+      </div>
+        <ArticleList articles = {this.state.reverted ? articles.slice().reverse() : articles}/>
+      </div>
+    )
+  }
+  revert = () => {
+    this.setState ({
+      reverted: !this.state.reverted
+    })
+  }
+}
+
+export default App
